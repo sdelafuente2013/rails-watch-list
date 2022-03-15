f604504c083d43bdb82a253e0499ca9b573f3a04
santi <santiagodelafuente2013@gmail.com>
2022-03-15 16:46:09 -0300

    frontend list ok

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 class BookmarksController < ApplicationController
-	def new
-		@bookmark = Bookmark.new
-	end 
+  def new
+    @bookmark = Bookmark.new
+  end
 
-	def create 
-		@bookmark = Bookmark.new(list_params)
-		@bookmark.list = List.find(params[:id])
+  def create
+    @bookmark = Bookmark.new(list_params)
+    @bookmark.list = List.find(params[:id])
 
-		if @bookmark.save
-			redirect_to list_path
-		else
-		    render :new, status: :unprocessable_entity
-		end
-	end 
-
-	private
-
-	def list_params
-      params.require(:bookmark).permit(:comment, :movie_id)
+    if @bookmark.save
+      redirect_to list_path
+    else
+      redirect_back_or_to lists_path, notice: "#{@bookmark.errors.full_messages.to_sentence}"
     end
+  end
+
+  private
+
+  def list_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,33 +1,39 @@
+# frozen_string_literal: true
+
 class ListsController < ApplicationController
-	def index
-		@lists = List.all
-		@movies = Movie.all
-	end 
 
-	def new
-		@list_new = List.new
-	end
+  def index
+    @lists = List.all
+  end
 
-	def create
-		@list_new = List.new(list_params)
+  def new
+    @list_new = List.new
+  end
 
-		if @list_new.save
-	  	redirect_to lists_path
-	  else
-	    render :new, status: :unprocessable_entity
-	  end
-	end 
+  def create
+    @list_new = List.new(list_params)
 
-	def show
-		@list_find = List.find(params[:id])
-		@bookmarks = Bookmark.all
-		@bookmark = Bookmark.new
-		@reviews = Review.all
-		@review = Review.new
-	end 
-
-	private
-    def list_params
-      params.require(:list).permit(:name)
+    if @list_new.save
+      redirect_to lists_path
+    else
+      # render :new, status: :unprocessable_entity
+      redirect_back_or_to lists_path, notice: "#{@list_new.errors.full_messages.to_sentence}"
     end
+  end
+
+  def show
+    @list_find = List.find(params[:id])
+
+    @bookmarks = Bookmark.all
+    @reviews = Review.all
+
+    @bookmark = Bookmark.new
+    @review = Review.new
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
